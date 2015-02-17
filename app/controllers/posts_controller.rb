72af633d70c4e14b70f13220b2c3af41a34aadee
Tho Nguyen <thogotmail@gmail.com>
2015-02-17 14:08:56 -0600

    update

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,48 @@
 class PostsController < ApplicationController
-  def index
+  before_action :set_post, only: [:edit, :update, :show]
 
+  def index
+    @posts = Post.all
+  end
+
+  def show
+    @comment = Comment.new
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+    @post.creator = User.first #temp
+    if @post.save
+      flash[:notice] = "Your post has been created"
+      redirect_to posts_path
+    else
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @post.update(post_params)
+      flash[:notice] = "Your post has been updated"
+      redirect_to post_path(@post)
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :url, :description, category_ids: [])
+  end
+
+  def set_post
+    @post = Post.find(params[:id])
   end
 end
