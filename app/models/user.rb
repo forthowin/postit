edd99c502b213bad726b71293068c943d45f994f
Tho Nguyen <thogotmail@gmail.com>
2015-02-20 20:31:44 -0600

    Add password confirmation

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,16 @@ class User < ActiveRecord::Base
   has_secure_password validations: false
 
   validates :username, presence: true, uniqueness: true
-  validates :password, presence: true, length: {minimum: 5}, on: :create
-  validates :password, length: {minimum: 5}, allow_blank: true, on: :update
+  validates :password, confirmation: true, presence: true, length: {minimum: 5}, on: :create
+  validates_confirmation_of :password_confirmation, on: :create
+  validates :password, confirmation: true, length: {minimum: 5}, allow_blank: true, on: :update
+  validate  :password_does_not_match_password_confirmation, on: :update
+
+  private
+
+  def password_does_not_match_password_confirmation
+    if password.blank? and !password_confirmation.blank?
+      errors.add("Password_confirmation_doesn't_match_Password", '')
+    end
+  end
 end
