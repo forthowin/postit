b9770ac9213c83c49cba152c4415577f166a0e1d
Tho Nguyen <thogotmail@gmail.com>
2015-02-24 14:37:29 -0600

    Add slug to URL

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,8 +11,37 @@ class User < ActiveRecord::Base
   validates :password, confirmation: true, length: {minimum: 5}, allow_blank: true, on: :update
   validate  :password_does_not_match_password_confirmation, on: :update
 
+  before_save :generate_slug
+
   def to_param
-    username
+    self.slug
+  end
+
+  def generate_slug
+    the_slug = to_slug(self.username)
+    user = User.find_by slug: the_slug
+    count = 2
+    while user and user != self
+      the_slug = append_suffix(the_slug, count)
+      user = User.find_by slug: the_slug
+      count += 1
+    end
+    self.slug = the_slug.downcase
+  end
+
+  def append_suffix(str, count)
+    if str.split('-').last.to_i != 0
+      return str.split('-').slic(0...-1).join('-') + '-' + count.to_s
+    else
+      return str + '-' + count.to_s
+    end
+  end
+
+  def to_slug(name)
+    str = name.strip
+    str.gsub!(/\s*[^A-Za-z0-9]\s*/, '-')
+    str.gsub!(/-+/,'-')
+    str.downcase
   end
 
   private
